20df48827f50d7a0e31a0e753c64165bc38854cb
karry95 <karrz95@gmail.com>
2016-11-18 10:20:58 +0000

    readcommit,

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement* speed;
+        
+    }
+   
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag ("Cube"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/gains.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gains : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(new Vector3(30,45) * Time.deltaTime);
+    }
+}
